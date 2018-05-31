0dee249445165e9868a862f94da6b51e20bd54d5
1995wangxiaowei <1404198521@qq.com>
2018-05-31 12:35:16 +0800

    第二次传送项目

--- js/index.js
+++ js/index.js
@@ -14,4 +14,60 @@ $(function(){
 				},function(){
 					$(".he").css("display","none")
 				})
-			})
+			})	
+			$(function(){
+				 var $div= $(".img2 ul div");
+				 $.each($div, function(){
+					$(this).hover(function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 250,height :250},200)
+					},function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 240,height :240},200)
+						})
+					})
+				 })
+			$(function(){
+				 var $div= $(".img3 ul div");
+				 $.each($div, function(){
+					$(this).hover(function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 250,height :250},200)
+					},function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 240,height :240},200)
+						})
+					})
+				 })
+			$(function(){
+				 var $div= $(".img4 ul div");
+				 $.each($div, function(){
+					$(this).hover(function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 250,height :250},200)
+					},function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 240,height :240},200)
+						})
+					})
+				 })
+
+			$(function(){
+				 var $div= $(".img5 ul div");
+				 $.each($div, function(){
+					$(this).hover(function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 250,height :250},200)
+					},function(){
+						var $img = $(this).find("img");
+						$img.animate({width : 240,height :240},200)
+						})
+					})
+				 })
+
+			$(function(){
+				  $("#top").click(function() {
+				      $("html,body").animate({Top:0},100);
+				  }); 
+				 })
+			
