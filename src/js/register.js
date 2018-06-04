40371b367cc7452b5260dba84ce57f74a9784c34
1995wangxiaowei <1404198521@qq.com>
2018-06-04 18:07:37 +0800

    第五次传送项目

--- src/js/register.js
+++ src/js/register.js
@@ -4,6 +4,7 @@
 			var oCon1 = document.getElementById("con1");
 			var oCon2 = document.getElementById("con2");
 			var oCon3 = document.getElementById("con3");
+			var oCon4 = document.getElementById("con4")
 	
 			oTxt1.onblur = function(){
 				var str = oTxt1.value;
@@ -38,5 +39,34 @@
 				}
 			
 			}
+			
+	var codeV = document.getElementById('code');
+   	function createCode(){
+        //首先默认code为空字符串
+        code = '';
+        //设置长度，这里看需求，我这里设置了4
+        var codeLength = 4;       
+        //设置随机字符
+        var random = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R', 'S','T','U','V','W','X','Y','Z');
+        //循环codeLength 我设置的4就是循环4次
+        for(var i = 0; i < codeLength; i++){
+            //设置随机数范围,这设置为0 ~ 6
+             var index = Math.floor(Math.random()*36);
+             //字符串拼接 将每次随机的字符 进行拼接
+             code += random[index]; 
+        }
+        //将拼接好的字符串赋值给展示的Value
+        codeV.value = code;
+    }
+   	var oTxt4 = document.getElementById("txt4");
+   	oTxt4.onblur = function(){   		
+   		if(codeV.value ===oTxt4.value){
+   			oCon4.style.display="none";
+   		}else{
+   			oCon4.style.display="inline";
+   		}
+   	}
+   	
+   	
 			
 			
